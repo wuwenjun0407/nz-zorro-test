2a475dde4026f11835c6d7c1cd39484c5f1bc4c8
wuwenjun0407 <451795057@qq.com>
2021-08-20 17:33:21 +0800

    fix(*): 90

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'src/**/*{js,jsx,ts,tsx,md,html}': ['prettier --write', 'npm run lint:ts'],
+    'src/**/*{js,jsx,ts,tsx,md,html}': ['npm run lint:ts', 'pretty-quick --staged'],
     'src/**/*.less': ['npm run lint:style', 'git add']
 };
