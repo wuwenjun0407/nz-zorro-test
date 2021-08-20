ef35b233602ed18b540af622c1ed0dd3d275ab98
wuwenjun0407 <451795057@qq.com>
2021-08-20 10:32:28 +0800

    fix(*):3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
@@ -7,7 +7,7 @@ import { Component ,OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 	title = 'nz-zorro-test';
-	ngOnInit(){
+	ngOnInit() {
 		console.log('111');
 	}
 }
