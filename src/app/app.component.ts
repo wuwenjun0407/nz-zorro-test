47a3ffafee00ee000c066e4f43d104bfedb724be
wuwenjun0407 <451795057@qq.com>
2021-08-20 16:53:47 +0800

    fix(*): yu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-     selector: 'app-root',
+    selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'nz-zorro-test';
+    ngOnInit() {
+        console.error('111');
+    }
 }
