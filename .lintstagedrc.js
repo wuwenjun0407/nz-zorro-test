775c70b3c4a03dbf4b8ccbb0745488c669bccb57
wuwenjun0407 <451795057@qq.com>
2021-08-20 17:35:57 +0800

    fix(*): 6786

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
     'src/**/*{js,jsx,ts,tsx,md,html}': ['npm run lint:ts', 'pretty-quick --staged'],
-    'src/**/*.less': ['npm run lint:style', 'git add']
+    'src/**/*.less': ['npm run lint:style', 'git add'],
+    '*': ['pretty-quick --staged']
 };
